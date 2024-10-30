a82f6a1b1828e7851b56755ea67efcad97e6d39b
Jacob Searle <jacobsearle.dev@gmail.com>
2024-10-30 09:45:45 -0400

    Updated paths to relative from absolute

--- scripts/Python_Basic_ETL.py
+++ scripts/Python_Basic_ETL.py
@@ -2,12 +2,11 @@ import pandas as pd
 
 #Step 1: Extract
 def extract_sales():
-    #Getting an error when I don't use the absolute path to the CSV, attempting to debug
-    sales_df = pd.read_csv('/Users/jakesearle/Projects/Data_Engineering_Projects/data/raw/sales.csv')
+    sales_df = pd.read_csv('../Data_Engineering_Projects/data/raw/sales.csv')
     return sales_df
 
 def extract_users():
-    users_df = pd.read_csv('/Users/jakesearle/Projects/Data_Engineering_Projects/data/raw/users.csv')
+    users_df = pd.read_csv('../Data_Engineering_Projects/data/raw/users.csv')
     return users_df
 
 
@@ -22,7 +21,7 @@ def transform_data(users_df, sales_df):
 
 #Step 3: Load
 def load_data(transformed_df):
-    transformed_df.to_csv('/Users/jakesearle/Projects/Data_Engineering_Projects/data/processed/total_sales.csv', index=False)
+    transformed_df.to_csv('../Data_Engineering_Projects/data/processed/total_sales.csv', index=False)
     print("Data loaded successfully!")
 
 #Main ETL Function
